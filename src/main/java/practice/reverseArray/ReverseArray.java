f80cba945817782e4587b264660aa02f160daaba
aleksandr_parshakov <sash29@yandex.ru>
2022-04-08 21:20:59 +0500

    Hospital done ReversArray done TwoDimensionalArray done

--- src/main/java/practice/reverseArray/ReverseArray.java
+++ src/main/java/practice/reverseArray/ReverseArray.java
@@ -4,6 +4,12 @@ public class ReverseArray {
 
     public static String[] reverse(String[] strings) {
         //TODO: Напишите код, который меняет порядок расположения элементов внутри массива на обратный.
+
+        for (int i = 0; i < strings.length / 2; i++ ) {
+            String reverse = strings[i];
+            strings[i] = strings[strings.length - i - 1];
+            strings[strings.length - i - 1] = reverse;
+        }
         return strings;
     }
 
